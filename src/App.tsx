dbac724b51f198c5a4dd83c803b19114280d6db1
김정환 <ej88ej@gmail.com>
2022-04-23 22:31:08 +0900

    useEffect 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import NameField1 from "./NameField1";
-import NameField2 from "./NameField2";
-import NameField3 from "./NameField3";
-import NameField4 from "./NameField4";
+import NameField1 from "./useState/NameField1";
+import NameField2 from "./useState/NameField2";
+import NameField3 from "./useState/NameField3";
+import NameField4 from "./useState/NameField4";
+import MyComponent1 from "./useEffect/MyComponent1";
+import MyComponent2 from "./useEffect/MyComponent2";
+import MyComponent3 from "./useEffect/MyComponent3";
+import MyComponent4 from "./useEffect/MyComponent4";
+import MyComponent5 from "./useEffect/MyComponent5 ";
+import MyComponent6Wrapper from "./useEffect/MyComponent6";
 
 function App() {
   return (
     <>
-      <NameField1 />
-      <NameField2 />
-      <NameField3 />
-      <NameField4 />
+      {/* <NameField1 /> */}
+      {/* <NameField2 /> */}
+      {/* <NameField3 /> */}
+      {/* <NameField4 /> */}
+      {/* <UseEffect1 /> */}
+      {/* <MyComponent1 /> */}
+      {/* <MyComponent2 /> */}
+      {/* <MyComponent3 /> */}
+      {/* <MyComponent4 /> */}
+      {/* <MyComponent5 /> */}
+      <MyComponent6Wrapper />
     </>
   );
 }
